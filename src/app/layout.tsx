376663cdc04b78f21daad897ecf8c232260839b6
Faith Osabutey (aka Fetty) <osabuteyfaith@gmail.com>
2026-06-10 17:48:53 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=DM+Sans:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body className="font-satoshi bg-sky-50 text-navy antialiased">
+      <body>
         <Providers>{children}</Providers>
       </body>
     </html>
