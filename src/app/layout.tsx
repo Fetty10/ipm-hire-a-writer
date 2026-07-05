847031c8513175ef12f3d5d4603273de559990fd
Faith Osabutey (aka Fetty) <osabuteyfaith@gmail.com>
2026-07-05 08:50:22 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 
 const syne = Syne({
   subsets: ["latin"],
-  weight: ["400", "600", "700"],
+  weight: ["400", "600", "700", "800"],
   variable: "--font-syne",
   display: "swap",
 });
